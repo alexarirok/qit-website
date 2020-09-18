a784678b34b2fa4f5bd44537b00b5afe7902e713
alexarirok <akorir233@gmail.com>
2020-09-18 17:23:25 +0300

    first commit

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from main import views 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name = 'about'),
+    path('base/', views.base, name = 'base'),
+    path('services/', views.services, name = 'services'),
+    path('contact/', views.contact, name = 'contact'),
+    path('application/', views.application, name = 'application'),
+]
---
+++ main/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from main.forms import ContactForm, ApplicationForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+
+def home(request):
+    template = 'home.html'
+    return render(request, template, { })
+
+def about(request):
+    template = 'about.html'
+    return render(request, template, { })
+
+def contact(request):
+    template = 'contacts.html'
+    if request.method == 'POST':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            F_name  = form.cleaned_data['F_name']
+            L_name  = form.cleaned_data['L_name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, email, ['kipkoechk38@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('sucess') 
+    return render(request, template, {'form':form})
+
+def application(request):
+    template = 'application.html'
+    if request.method == 'POST':
+        form = ApplicationForm()
+    else:
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            course = form.cleaned_data['course']
+            F_name  = form.cleaned_data['name']
+            M_name  = form.cleaned_data['name']
+            L_name  = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            phone_num = form.cleaned_data['phone_num']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, email, ['kipkoechk38@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('sucess') 
+    return render(request, template, {'form':form})
+
+def services(request):
+    template = 'services.html'
+    return render(request, template, { })
+
+def base(request):
+    template = 'base.html'
+    return render(request, template, { })
+# Create your views here.
